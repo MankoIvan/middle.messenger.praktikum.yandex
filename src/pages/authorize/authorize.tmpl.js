63ad52ac5b54b31768a37a3042e118205725199b
Иван Манько <ivanmanko@MacBook-Pro-Ivan.local>
2021-07-10 01:40:29 +0300

    all fixes, but a new branch

---
+++ src/pages/authorize/authorize.tmpl.js
@@ -0,0 +1,26 @@
+import { formPiece } from "../../modules/input/formPiece";
+
+const authorizeTmpl = `
+    <div class="form">
+        <h1 class="form__title">Вход</h1>
+        <form class="form__container">
+            ${formPiece({
+                name: "login",
+                label: "Логин",
+                type: "text",
+                message: "Неверный логин",
+            })}
+            ${formPiece({
+                name: "password",
+                label: "Пароль",
+                type: "password",
+                message: "Неверный пароль",
+            })}
+        </form>
+        <div class="form__buttons">
+            <button class="button_main" type="submit">Авторизоваться</button>
+            <button class="button" type="button">Нет аккаунта?</button>
+        </div>
+    </div>
+`;
+export { authorizeTmpl };
