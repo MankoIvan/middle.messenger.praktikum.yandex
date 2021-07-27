100f7d92c00b67bc622bf9ff692d274b76a1ff08
Manko Ivan <inoi2002@gmail.com>
2021-07-28 00:12:23 +0300

    components extend Block

--- src/pages/error/error.ts
+++ src/pages/error/error.ts
@@ -1,16 +1,6 @@
 import Handlebars from "handlebars";
-import Block from "../../modules/block";
+
 import { errorTmpl } from "./error.tmpl";
-
-class Error extends Block {
-  constructor() {
-    super("container", { code: string, message: string });
-  }
-}
-
-
-
-
 
 const template = Handlebars.compile(errorTmpl);
 
