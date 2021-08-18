aec2ed99ef84271e58d8b182d35f8fcf1c1edcbe
Mankoivan <inoi2002@gmail.com>
2021-08-18 16:51:19 +0300

    avatar not successfull

--- src/pages/user/user.tmpl.ts
+++ src/pages/user/user.tmpl.ts
@@ -1,9 +1,10 @@
 const userTmpl = `
     <div class="user">
         <h1 class="user__title">Профиль</h1>
-        <div class="user__img">
+        <img class="user__img" {{#if userData.avatar}}src={{userData.avatar}}{{/if}}>
                 <p class="user__img-coverText">Изменить<br>аватар</p>
-        </div>
+        </img>
+        //<input type="file" id="userAvatar" class="user__avatarInput">
         <form name="userForm" class="form__container">
           {{{emailInput}}}
           {{{loginInput}}}
